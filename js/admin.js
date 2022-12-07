cc423bbbfebec80420a67fbe20f848d1d149cb51
SethJKallis <kallisseth@gmail.com>
2022-12-07 02:20:49 +0200

    beginning of js implementation

--- js/admin.js
+++ js/admin.js
@@ -3,19 +3,31 @@ fetch('../data/data.json')
     return response.json()
 })
 .then((data) => {
-    let tableContent = document.querySelector('tbody');
-    Object.keys(data).forEach((item) => {
-        if(data[item]){
+    localStorage.setItem('myDataStore', JSON.stringify(data));
+    
+})
+
+let tableContent = document.querySelector('tbody');
+    let myData = JSON.parse(localStorage.getItem('myDataStore'));
+    Object.keys(myData).forEach((item) => {
+        if(myData[item]){
             tableContent.innerHTML += 
             `
             <tr>
-                <td>${data[item].id}</td>
-                <td>${data[item].gameTitle}</td>
-                <td>R${data[item].price}</td>
+                <td>${myData[item].id}</td>
+                <td>${myData[item].gameTitle}</td>
+                <td>R${myData[item].price}</td>
                 <td><button id="editBtn" class="btn btn-success">Edit</button></td>
                 <td><button id="deleteBtn" class="btn btn-danger">Del</button></td>
             </tr>
             `
         }
     })
-})
+    
+    let edit = document.querySelectorAll('.btn-success');
+    console.log(edit)
+    Object.keys(edit).forEach((item) => {
+        edit[item].addEventListener('click', (e) => {
+            console.log(item)
+        })
+    })
